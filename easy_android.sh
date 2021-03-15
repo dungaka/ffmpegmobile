9a7f6f4a54b4de3b9735945fd97a7fb9b8bfca63
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-03-15 18:10:34 +0800

    fix libpng build

--- easy_android.sh
+++ easy_android.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# fix .git repo not exists
+# BUILD_VERSION=$(git describe --tags)
+
+# fix libpng 192.168.31.115 build error (link libz)
+# https://github.com/LuaDist/libpng
+
 ./android.sh --disable-arm-v7a --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass --reconf-fribidi --reconf-expat --reconf-fontconfig 
 # --skip-ffmpeg
 
