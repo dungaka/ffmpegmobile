9ad2654667923fac10a030d6d291ee6b89721a18
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-03-15 11:06:18 +0800

    add easy android build script

---
+++ easy_android.sh
@@ -0,0 +1,4 @@
+!/bin/bash
+
+./android.sh --disable-arm-v7a --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass --reconf-fribidi --reconf-expat --reconf-fontconfig
+
