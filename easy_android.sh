29cd2b18faf4cdc2f4a05380092b2855b371f1cc
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-03-15 15:15:19 +0800

    little change

--- easy_android.sh
+++ easy_android.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
-./android.sh --disable-arm-v7a --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass --reconf-fribidi --reconf-expat --reconf-fontconfig
+./android.sh --disable-arm-v7a --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass --reconf-fribidi --reconf-expat --reconf-fontconfig --skip-ffmpeg
 
