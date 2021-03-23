5b81e2cdec39efb7b43f1f4a47bef6c5ae8008ef
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-03-23 12:01:00 +0800

    mod easy_android.sh

--- easy_android.sh
+++ easy_android.sh
@@ -9,6 +9,16 @@
 # fix libpng 192.168.31.115 build error (link libz)
 # https://github.com/LuaDist/libpng
 
-./android.sh --disable-arm-v7a --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass --reconf-fribidi --reconf-expat --reconf-fontconfig 
-# --skip-ffmpeg
+unset PKG_CONFIG_PATH
+unset C_INCLUDE_PATH
+
+export ANDROID_NDK_ROOT=$NDK_HOME
+
+sed -i 's/compileSdkVersion 29/compileSdkVersion 30/g' tools/release/android/build.lts.gradle
+sed -i 's/compileSdkVersion 29/targetSdkVersion 26/g' tools/release/android/build.lts.gradle
+
+./android.sh --lts --disable-x86 --disable-x86-64 --enable-gpl --enable-x264 --enable-android-media-codec --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass
+#--enable-gnutls
+#--reconf-fribidi --reconf-expat --reconf-fontconfig 
+#--skip-ffmpeg
 
