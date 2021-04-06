decedca4ea193327582a42e96efd7ee0105c0928
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-04-06 12:04:08 +0800

    add curl upload cmd

--- easy_ios.sh
+++ easy_ios.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# curl -T ios_lts_lite_x264_videotoolbox_ass.zip ftp://BBBBBBB:AAAAAAAAAAAA@ilovejoke.tk:9121//AiDisk_a1/opt/wwwroot/default/ -v
+
 ./ios.sh --force --lts --disable-i386 --enable-gpl --enable-x264 --enable-ios-videotoolbox --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass
+
