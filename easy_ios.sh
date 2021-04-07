5c567870cdfd3440ac250c936226e40fb47cd95a
chenjian <chenj@52iuh.com>
2021-04-07 14:06:34 +0800

    easy_ios.sh +x

--- easy_ios.sh
+++ easy_ios.sh
@@ -2,5 +2,5 @@
 
 # curl -T ios_lts_lite_x264_videotoolbox_ass.zip ftp://BBBBBBB:AAAAAAAAAAAA@ilovejoke.tk:9121//AiDisk_a1/opt/wwwroot/default/ -v
 
-./ios.sh --force --lts --disable-i386 --enable-gpl --enable-x264 --enable-ios-videotoolbox --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass
+./ios.sh --force --lts --enable-gpl --enable-x264 --enable-ios-videotoolbox --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass
 
