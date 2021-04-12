5ce522db43ec9eea7cc967fe13d0be60e688f14d
ubuntu1804_wsl <wnpllr@ubuntu1804.com>
2021-04-12 11:27:30 +0800

    update version to 1.0.5

--- android/app/src/main/cpp/mobileffmpeg.h
+++ android/app/src/main/cpp/mobileffmpeg.h
@@ -28,7 +28,7 @@
 
 /** Library version string */
 //#define MOBILE_FFMPEG_VERSION "4.4"
-#define MOBILE_FFMPEG_VERSION "1.0.4"
+#define MOBILE_FFMPEG_VERSION "1.0.5"
 
 /** Defines tag used for Android logging. */
 #define LIB_NAME "mobile-ffmpeg"
