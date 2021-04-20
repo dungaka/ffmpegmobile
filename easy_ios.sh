5c5d05c43cb020e002dd9d4af82296649a60b447
chenjian <chenj@52iuh.com>
2021-04-20 16:47:30 +0800

    add gnutls pod notice

--- easy_ios.sh
+++ easy_ios.sh
@@ -2,5 +2,8 @@
 
 # curl -T ios_lts_lite_x264_videotoolbox_ass.zip ftp://BBBBBBB:AAAAAAAAAAAA@ilovejoke.tk:9121//AiDisk_a1/opt/wwwroot/default/ -v
 
+# gnutls podspec
+# 'ios-framework/gmp.framework', 'ios-framework/gnutls.framework', 'ios-framework/libhogweed.framework', 'ios-framework/libnettle.framework'
+
 ./ios.sh --force --lts --enable-gpl --enable-x264 --enable-ios-videotoolbox --enable-fontconfig --enable-freetype --enable-fribidi --enable-libass
 
